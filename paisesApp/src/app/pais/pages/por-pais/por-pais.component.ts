690156d225778505b8247ace9c2bae0fa2c57be1
danieladbsnt <daniela.darnea@bosonit.com>
2022-11-23 17:42:44 +0100

    added component html and buscar function for fetch in paisService

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino: string = ''
+  hayError: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino)
+    //ponemos el subscribe aqui porque no queremos retornar la info en el servicio, sino que queremos retornarla donde se llamó a buscarPais()
+    .subscribe({
+      next: (paises) => {
+        console.log(paises);
+      },
+      error: (err) => {
+        this.hayError = true;
+      }
+    })
   }
 
 }
+
+//Así lo hace en el vídeo, pero con esta versión de angular han cambiado cosas y por eso lo hacemos diferente
+// .subscribe(resp) => {
+//   console.log(resp);
+// }, (err) => {
+//   this.hayError = true;
+// }
