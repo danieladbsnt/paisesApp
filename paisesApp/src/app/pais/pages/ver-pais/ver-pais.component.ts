b1838b20e0bcf0f78fe2a9b6bb3ac6db8857c90f
danieladbsnt <daniela.darnea@bosonit.com>
2022-11-24 17:45:21 +0100

    added ver pais component functionality

--- paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from "rxjs/operators";
+import { Country } from '../../interface/pais.interface';
+
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
 
   ngOnInit(): void {
+
+  this.activatedRoute.params
+  .pipe(
+    switchMap(({id}) => this.paisService.buscarPaisPorId(id)),
+    tap(console.log)
+  )
+    .subscribe(pais => this.pais = pais[0])
   }
 
 }
+
+/* Otra forma de hacerlo: 
+leemos los parametros que vienen en el url, extraemos el id y ese id es el que tenemos que mandar a buscarPaisPorId. Este observable retorna un pais.
+this.activatedRoute.params
+    .subscribe(({id}) => {
+      console.log(id);
+      this.paisService.buscarPaisPorId(id)
+      .subscribe(pais => {
+        console.log(pais);
+      })
+    })*/
