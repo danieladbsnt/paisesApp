7965a86b2e27ba10673449c376bfaf64cd2fe323
danieladbsnt <daniela.darnea@bosonit.com>
2022-11-25 14:56:52 +0100

    finished region functionality

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interface/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -13,13 +15,26 @@ export class PorRegionComponent {
 
   regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = '';
+  paises: Country[] = [];
+
+constructor(private paisService: PaisService) {}
 
   getClass(region: string): string {
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
   activarRegion(region: string) {
-    this.regionActiva = region;
+    this.regionActiva = region;  
+    
+    this.paisService.buscarRegion(region)
+    .subscribe({
+      next: (resp) => {
+        this.paises = resp;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 
 }
